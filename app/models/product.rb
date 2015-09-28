e15bde1fc9bf4687a2d117d9edb4aaeb62dfc4d7
rajat <rajat@vinsol.com>
2015-09-28 20:04:28 +0530

    catalog display

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,9 @@ validates :image_url, allow_blank: true, format: {
   %r{\.(gif|jpg|png)\Z}i,
   message: 'must be a URL for GIF, JPG or PNG image.'
 }
+
+def self.latest
+Product.order(:updated_at).last
+end
+
 end
